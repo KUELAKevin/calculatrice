56f749ba2c55e8ff195b37a7253501fcc69eb52c
Pégué Judicaël OUEDRAGO <pegueo2000@gmail.com>
2023-07-06 12:53:02 +0000

    finalisation du fichier calculatrice.py avec la saisie des nombres et correction de l'aspect du code

--- calculatrice.py
+++ calculatrice.py
@@ -1,20 +1,28 @@
+import maths
+import menu
+
+print("saisie du premier nombre")
+a = maths.saisir_nombre()
+print("saisie du deuxieme nombre")
+b = maths.saisir_nombre()
+menu.affiche_menu()
 while True:
-        try:
-            n =int(input("Saisir le nombre correspondant au choix \n"))
-            if(n==1):
-                 addition= maths.addition(a,b)
-                 print(f"{a}+{b}={addition}")
-            elif(n==2):
-                 soustraction = maths.soustraction(a,b)
-                 print(f"{a}-{b}={soustraction}")
-            elif(n==3):
-                 multiplication = maths.multiplication(a,b)
-                 print(f"{a}*{b}={multiplication}")
-            elif(n==4):
-                 division = maths.division(a,b)
-                 print(f"{a}/{b}={division}")
-            else:
-                 print("Mauvais choix")
-            break
-        except ValueError:
-            print("saisir un nombre valide")
+    try:
+        n = int(input("Saisir le nombre correspondant au choix \n"))
+        if n == 1:
+            addition = maths.addition(a, b)
+            print(f"{a}+{b}={addition}")
+        elif n == 2:
+            soustraction = maths.soustraction(a, b)
+            print(f"{a}-{b}={soustraction}")
+        elif n == 3:
+            multiplication = maths.multiplication(a, b)
+            print(f"{a}*{b}={multiplication}")
+        elif n == 4:
+            division = maths.division(a, b)
+            print(f"{a}/{b}={division}")
+        else:
+            print("Mauvais choix")
+        break
+    except ValueError:
+        print("saisir un nombre valide")
